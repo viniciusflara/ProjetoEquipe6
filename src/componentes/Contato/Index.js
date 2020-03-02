9a79768443ea1022d196e8bd3beccd94512ef154
Joao <joaopedrocancado@gmail.com>
2020-03-01 21:17:42 -0300

    alteracoes na pagina de contato

--- src/componentes/Contato/Index.js
+++ src/componentes/Contato/Index.js
@@ -1,15 +1,10 @@
 import React, { useRef } from 'react';
 import './Index.css';
-import { Label } from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
 import * as Yup from 'yup';
 import Input from '../Form/Input';
 import { Form } from '@unform/web';
 
-
-
-const initialData = {
-    email: 'example@gmail.com',
-}
 
 function Contato() {
     const formRef = useRef(null);
@@ -44,24 +39,25 @@ function Contato() {
         }
     }
     return (
-        <>
-            <div id="contato2" className="contato mt-5">
-                <h1>Contato</h1>
-            </div>
-            <div id="contato" className="contato mt-5">
-                <h1>Entrar em Contato</h1>
-                <p>Entre em contato conosco para solicitar sua cotação, se tiver alguma dúvida ou para falar um oi.</p>
-                <Form ref={formRef} initialData={initialData} onSubmit={handleSubmit}>
-                    <Input name="name" label="Nome" />
-                    <Input type="email" name="email" label="E-mail" />
-                    <Input type="tel" name="fone" label="Telefone" />
-                    <label for="mensagem">Mensagem</label>
-                    <textarea id="mensagem" name="mensagem" rows="10" />
+        <div className="contato mt-5">
+            <Row>
+                <img src="./img/contato1.png" />
+                <div id="sla">
+                <h1 className="titulo">Tire seu site do papel.<span className="titulo2">Entre em contato com a gente.</span></h1>
+                <p>Retornamos o mais rápido possível.</p>
+                </div>
+            </Row>
+            
+            <Form ref={formRef} onSubmit={handleSubmit}>
+                <Input id="input1" name="name" placeholder="Nome" />
+                <Input id="input2" type="email" name="email" placeholder="E-mail" />
+                <Input id="input3" type="tel" name="fone" placeholder="Telefone" />
+                <textarea id="mensagem" name="mensagem" rows="10" placeholder="Mensagem" />
+                <button type="submit">Enviar Mensagem</button>
+            </Form>
+            <img className="img2" src="./img/contato2.png" />
+        </div>
 
-                    <button type="submit">Enviar Mensagem</button>
-                </Form>
-            </div>
-        </>
     );
 
 }
