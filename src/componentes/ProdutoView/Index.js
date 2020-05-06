9cd677d61c5d12a473179564995d4e379071b8c4
Vitor <vmarquesds@gmail.com>
2020-05-06 19:17:09 -0300

    criação do filtro

--- src/componentes/ProdutoView/Index.js
+++ src/componentes/ProdutoView/Index.js
@@ -3,9 +3,35 @@ import './Index.css';
 
 function ProdutoView(){
     return(
-        <di className="ProdutoView">
-            <h1>ProdutoView</h1>
-        </di>
+        <div className="ProdutoView">
+           <div className="Filtro">
+               <div className="Filtro-Nome">
+                   <h3>Nome: </h3>
+                   <input type="text" name="nome"></input>
+               </div>
+               <div className="Filtro-Material">
+                   <h3>Material: </h3>
+                   <input type="text" name="material"></input>
+               </div>
+               <div className="Filtro-Tipo">
+                   <h3>Tipo: </h3>
+                   <p><input type="radio" name="armas" value="armas" />Armas</p>
+                   <p><input type="radio" name="armaduras" />Armaduras</p>
+                   <p><input type="radio" name="equipamentos" />Equipamentos</p>
+               </div>
+               <div className="Filtro-Subtipo">
+                   <h3>SubTipo: </h3>
+                   <p><input type="radio" name="c-a-c" />Armas corpo a corpo</p>
+                   <p><input type="radio" name="a-a-d" />Armas a distancia</p>
+                   <p><input type="radio" name="a-l" />Armaduras leves</p>
+                   <p><input type="radio" name="a-p" />Armaduras pesadas</p>
+                   <p><input type="radio" name="escudos" />Escudos</p>
+               </div>
+
+           </div>
+
+
+        </div>
     );
 }
 
