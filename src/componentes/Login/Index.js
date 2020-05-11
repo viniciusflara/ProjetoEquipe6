8a0f2eba12986c3f8c0d681267307b8edd9bca3c
Vitor <vmarquesds@gmail.com>
2020-05-11 17:52:27 -0300

    Revert "Merge remote-tracking branch 'origin/login'"
    This reverts commit 20c6fc49191a592a7c4976a7fb496feb1618ddd0.

--- src/componentes/Login/Index.js
+++ src/componentes/Login/Index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Index.css';
-import Corpo from "./corpo";
-
 
 function Login(){
     return(
-        <div class='container'>
-            <Corpo />
-        </div>
+        <di className="Login">
+            <h1>Login</h1>
+        </di>
     );
 }
 
