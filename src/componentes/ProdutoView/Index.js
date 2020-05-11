c68c626cc5e738d64bf23b1e3b0fc47548a8be20
Vitor <vmarquesds@gmail.com>
2020-05-11 19:47:08 -0300

    minor changes

--- src/componentes/ProdutoView/Index.js
+++ src/componentes/ProdutoView/Index.js
@@ -15,17 +15,17 @@ function ProdutoView(){
                </div>
                <div className="Filtro-Tipo">
                    <h3>Tipo: </h3>
-                   <p><input type="radio" name="armas" value="armas" />Armas</p>
-                   <p><input type="radio" name="armaduras" />Armaduras</p>
-                   <p><input type="radio" name="equipamentos" />Equipamentos</p>
+                   <p><input type="checkbox" name="armas" value="armas" />Armas</p>
+                   <p><input type="checkbox" name="armaduras" />Armaduras</p>
+                   <p><input type="checkbox" name="equipamentos" />Equipamentos</p>
                </div>
                <div className="Filtro-Subtipo">
                    <h3>SubTipo: </h3>
-                   <p><input type="radio" name="c-a-c" />Armas corpo a corpo</p>
-                   <p><input type="radio" name="a-a-d" />Armas a distancia</p>
-                   <p><input type="radio" name="a-l" />Armaduras leves</p>
-                   <p><input type="radio" name="a-p" />Armaduras pesadas</p>
-                   <p><input type="radio" name="escudos" />Escudos</p>
+                   <p><input type="checkbox" name="c-a-c" />Armas corpo a corpo</p>
+                   <p><input type="checkbox" name="a-a-d" />Armas a distancia</p>
+                   <p><input type="checkbox" name="a-l" />Armaduras leves</p>
+                   <p><input type="checkbox" name="a-p" />Armaduras pesadas</p>
+                   <p><input type="checkbox" name="escudos" />Escudos</p>
                </div>
 
            </div>
