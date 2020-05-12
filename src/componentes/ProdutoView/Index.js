c9552c0cb569e053289cc2eac11abaaba58f7f32
Vitor <vmarquesds@gmail.com>
2020-05-12 08:51:02 -0300

    Pequena modularizada no produtoview

--- src/componentes/ProdutoView/Index.js
+++ src/componentes/ProdutoView/Index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Index.css';
 import { NavLink } from 'react-router-dom';
+import Filtro from './Filtro'
 
 const DATA = [{
     id: '123',
@@ -53,32 +54,7 @@ function ProdutoView() {
 
     return (
         <div className="ProdutoView">
-            <div className="showFiltro" id="showFiltro">
-                <div className="Filtro">
-                    <div className="Filtro-Nome">
-                        <h3>Nome: </h3>
-                        <input type="text" name="nome"></input>
-                    </div>
-                    <div className="Filtro-Material">
-                        <h3>Material: </h3>
-                        <input type="text" name="material"></input>
-                    </div>
-                    <div className="Filtro-Tipo">
-                        <h3>Tipo: </h3>
-                        <p><input type="checkbox" name="armas" value="armas" />Armas</p>
-                        <p><input type="checkbox" name="armaduras" />Armaduras</p>
-                        <p><input type="checkbox" name="equipamentos" />Equipamentos</p>
-                    </div>
-                    <div className="Filtro-Subtipo">
-                        <h3>SubTipo: </h3>
-                        <p><input type="checkbox" name="c-a-c" />Armas corpo a corpo</p>
-                        <p><input type="checkbox" name="a-a-d" />Armas a distancia</p>
-                        <p><input type="checkbox" name="a-l" />Armaduras leves</p>
-                        <p><input type="checkbox" name="a-p" />Armaduras pesadas</p>
-                        <p><input type="checkbox" name="escudos" />Escudos</p>
-                    </div>
-                </div>
-            </div>
+            <Filtro />
             <div className="buttonRow">
                 <button className="btnFiltro" onClick={() => showFiltro(0)}>Filtro</button>
                 <NavLink activeClassName="chosen" exact to="/produtocrud" className="btnAdicionar">+ Adicionar Produto</NavLink>
