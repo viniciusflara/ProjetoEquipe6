ffc7b2db63587d60a2ae766d1885d996dfcb6780
André Versiani <lumini2@hotmail.com.br>
2020-05-15 10:16:40 -0300

    passei pra classes e coloquei um sistema basico de login

--- src/componentes/Login/Index.js
+++ src/componentes/Login/Index.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Index.css';
+import Corpo from './corpo';
+import data from './data';
 
-function Login(){
-    return(
-        <di className="Login">
-            <h1>Login</h1>
-        </di>
-    );
+class Login extends Component {
+    
+    constructor() {
+        super() 
+        
+        this.state = {
+            data: data,
+            usuario:'',
+            senha:''
+        }
+    }
+
+    
+    entrar = () => {
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        let validacao = false;
+        
+        this.state.data.map(dado => {
+            if (username === dado.username && password === dado.password) {
+                validacao = !validacao;
+                return validacao;
+        
+            }
+        })
+            validacao ? window.location.href = '/produtoview' : alert("Dados Incorretos");
+    }
+
+
+    cadastrar = () => window.location.href = '/usercrud';
+    
+    
+    render() {
+        return (
+            <Corpo 
+            entrar={this.entrar} 
+            cadastrar={this.cadastrar}
+            />
+        );
+    }
 }
 
 export default Login;
---
+++ src/componentes/Login/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        username:'madoke',
+        password:'123'
+    },
+    {
+        username:'lumini23',
+        password:22
+    }
+]
+
+export default data
