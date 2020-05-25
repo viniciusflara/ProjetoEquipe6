b07a87ef442711cad8bb8190d4a8fe617f03e26d
mariana <mariribeiro968@gmail.com>
2020-05-25 18:44:11 -0300

    cadastro

--- src/pages/UserCrud/Index.js
+++ src/pages/UserCrud/Index.js
@@ -1,11 +1,103 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './Index.css';
 
+// import { Form , Container } from './styled';
+
+class Usuario{
+    constructor(nome, classe,email,senha){
+        this.nome = nome;
+        this.classe = classe;
+        this.email = email;
+        this.senha = senha;
+    }
+}
+
+class Bd{
+
+    constructor(){
+        let id = localStorage.getItem('id');
+        if(id === null){
+            localStorage.setItem('id', 0);
+        }
+    }
+
+    getProximoId(){
+        let proximoId = localStorage.getItem('id');
+        return parseInt(proximoId) + 1;
+    }
+
+    gravar(u){
+        let id = this.getProximoId();
+        localStorage.setItem(id, JSON.stringify(u));
+        localStorage.setItem('id', id);
+    }
+
+}
+
+let bd = new Bd();
+
 function UserCrud(){
+    const [nome, setNome] = useState('');
+    const [classe, setClasse] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+        let usuario = new Usuario(nome,classe,email,senha);
+       bd.gravar(usuario);
+    }
+
     return(
-        <di className="UserCrud">
-            <h1>UserCrud</h1>
-        </di>
+    
+    <>
+    <div className="corpoUser">
+        <div className="UserCrud">
+            <h1>Crie sua conta</h1>
+        </div>
+        
+        <form onSubmit={handleSubmit}>
+            
+            <label htmlFor="nome">
+                Nome:
+                <input type="text" 
+                value={nome} 
+                onChange={e => setNome(e.target.value)}
+                placeholder="Seu nome"
+            />
+            </label>
+            <label htmlFor="classe">
+                Classe:
+                <input type="text" 
+                value={classe} 
+                onChange={e => setClasse(e.target.value)}
+                placeholder="Sua Classe"
+            />
+            </label>
+            <label htmlFor="email">
+                Email:
+                <input type="email" 
+                value={email} 
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Seu email"
+            />
+            </label>
+            <label htmlFor="senha">
+                Senha:
+                <input type="password" 
+                value={senha} 
+                onChange={e => setSenha(e.target.value)}
+                placeholder="Sua senha"
+            />
+            </label>
+            <button type="submit" >Criar minha conta</button>
+        {/* </Form> */}
+    {/* </Container> */}
+
+    </form>
+    </div> 
+    </>
     );
 }
 
